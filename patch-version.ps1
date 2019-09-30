08c3332726b70d30903ea7d239af617641381179
Lewis Kirkaldie <lewis@munich.cinegy.local>
2019-09-30 12:23:26 +0100

    -upgrade version patch script to work

--- patch-version.ps1
+++ patch-version.ps1
@@ -1,11 +1,50 @@
-Param(
-[Parameter(Mandatory=$True,Position=1)]
-[string]$buildCounter)
+# PowerShell Cinegy Build Script
+# COPYRIGHT Cinegy GmbH 2019
+Param([string]$BuildCounter=0,[string]$SourceRevisionValue="FFFFFF",[string]$OverrideMinorVersion="")
 
-$fileName = "./src/app/ws-configuration/ws-configuration.ts"
+#read major / minor version values from a file held in source control
+$majorVer=99
+$minorVer=99
 
-$regex = "\s*public\s+readonly\s+commit\s+=\s+'\d+';\s*"
+#define various regular expressions to be used when hunting in files
+$versionSniffingRegex = "(\s*#define\s+(\S+)\s+)(\d+)"
 
-$replacement = "`tpublic readonly commit = '$buildCounter'"
+Get-Content ".\version.h" |  Where-Object { $_ -match $versionSniffingRegex } | ForEach-Object {
+	switch ($Matches[2])
+	{
+		"VERSION_MAJOR" { $majorVer = $Matches[3] }
+		"VERSION_MINOR" { $minorVer = $Matches[3] }
+	}	
+}
 
-(Get-Content $fileName) -replace $regex, $replacement | Out-File $fileName
+if($OverrideMinorVersion)
+{
+    $minorVer = $OverrideMinorVersion
+}
+
+#calculte a UInt16 from the commit hash to use as 4th version flag
+$shortRev = $SourceRevisionValue.Substring(0,4)
+$sourceAsDecimal = [System.Convert]::ToUInt16($shortRev, 16)
+
+#find TS configuration files and update versions
+$MajorRegex = "(^\s*public readonly major\s*=\s*')(.*)(';)"
+$MinorRegex = "(^\s*public readonly minor\s*=\s*')(.*)(';)"
+$BuildRegex = "(^\s*public readonly build\s*=\s*')(.*)(';)"
+$CommitRegex = "(^\s*public readonly commit\s*=\s*')(.*)(';)"
+
+Get-ChildItem -Path "./src/app/*/ws-configuration.ts" -Recurse | ForEach-Object {
+    $fileName = $_
+    Write-Host "Processing metadata changes for file: $fileName"
+
+    $FileLines = Get-Content -path $fileName 
+    
+    for($i=0;$i -lt $FileLines.Count;$i++)
+    {
+        $FileLines[$i] = $FileLines[$i] -Replace $MajorRegex, "`${1}$majorVer`$3"
+        $FileLines[$i] = $FileLines[$i] -Replace $MinorRegex, "`${1}$minorVer`$3"
+        $FileLines[$i] = $FileLines[$i] -Replace $BuildRegex, "`${1}$BuildCounter`$3"
+        $FileLines[$i] = $FileLines[$i] -Replace $CommitRegex, "`${1}$sourceAsDecimal`$3"
+    }
+
+    $FileLines | Out-File $fileName.FullName
+}
