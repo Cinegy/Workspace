394c74d0d956594b022db18b079a8b4adbfa29ae
Alexey <alexey@rozanov.org>
2020-12-21 14:29:40 +0300

    BF 13930	Workspace: wrong thumbnail is displayed for sub-clip created in ClipBin BF 13944	Workspace: a number is not added to the name if a sub-clip is created from of a clip located in ClipBin BF 14532	Workspace: the license isn`t exempted after logout the program or users just closed a browser rather than log out

--- src/app/ws-logout/ws-logout.service.ts
+++ src/app/ws-logout/ws-logout.service.ts
@@ -2,19 +2,33 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { WsAppStateService } from '../ws-app-state.service';
 import { WsConfigurationService } from '../ws-configuration/ws-configuration.service';
+import {WsBaseMamService} from "../shared/services/ws-base-mam/ws-base-mam.service";
+import {HttpClient} from "@angular/common/http";
+import {WsMamError} from "../shared/services/ws-base-mam/ws-mam-error";
 
 @Injectable({
   providedIn: 'root'
 })
-export class WsLogoutService {
+export class WsLogoutService extends WsBaseMamService {
 
   public logoutSubject: Subject<any> = new Subject<any>();
 
-  constructor(private config: WsConfigurationService, private appState: WsAppStateService) { }
+  constructor(protected httpClient: HttpClient, protected appState: WsAppStateService) {
+    super(httpClient, appState);
+    this.logoutSubject.subscribe(response=>this.logoutResponse(response));
+  }
 
   public logout() {
+    this.post(`${this.appState.selectedMam.mamEndpoint}authentication/logout`, null, this.logoutSubject);
+//    this.appState.setConnectionState(false, null);
+//    this.logoutSubject.next(true);
+  }
+
+  private logoutResponse(response: any) {
+    if (response instanceof WsMamError) {
+      return;
+    }
     this.appState.setConnectionState(false, null);
-    this.logoutSubject.next(true);
   }
 }
 
